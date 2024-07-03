a627531d49be1159cf1422d615f6a82b5098fe8a
pavansk06 <pxkosuru@zonemail.clpccd.edu>
2024-07-02 23:33:44 -0700

    commit 1

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ def main():
     Code Your Program here
     ########################################
     """
+    while True:
+        try:
+            enumber = int(input('Enter a number'))
+        except ValueError:
+            print ('Invalid input: Value Error')
+            continue
+        else:
+            print()
 
     ########################################
     # Do not delete the return statement
